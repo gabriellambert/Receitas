7e9307aacd60e364c84c91bd87cd6ff9d132ffff
Gabriel Mussolin Lambert <gabriel.mussolin@gmail.com>
2019-12-10 09:26:29 -0300

    Cria banco de dados postgresql e integra ao projeto

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sorvete',
+        3: 'Sopa de Legumes',
+        4: 'Bolo de Chocolate'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    return render(request, 'index.html', dados)
 
 
 def receita(request):
